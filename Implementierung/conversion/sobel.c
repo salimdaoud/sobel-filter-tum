41c27f74ebec199f210d1cabe56c22d1fa4c3100
Luca <ge96hik@mytum.de>
2024-12-28 18:03:09 +0100

    added a posix standard to makefile and added basic time measurement to sobel.c

--- Implementierung/conversion/sobel.c
+++ Implementierung/conversion/sobel.c
@@ -1,5 +1,6 @@
 #include "sobel.h"
 #include "grayscale.h"
+#include <time.h>
 
 // Sobel operator implementation
 void sobel( const uint8_t* img, size_t width, size_t height,
@@ -25,6 +26,10 @@ void sobel( const uint8_t* img, size_t width, size_t height,
             {-1, -2, -1}
     };
 
+    // Start time measurement
+    struct timespec start;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
     for (size_t ppm_y = 0; ppm_y < height; ppm_y++) {
         for (size_t ppm_x = 0; ppm_x < width; ppm_x++) {
             int sum_vertical = 0, sum_horizontal = 0;
@@ -32,10 +37,10 @@ void sobel( const uint8_t* img, size_t width, size_t height,
             // Apply Sobel kernel to pixel
             for (int kern_y = -1; kern_y <= 1; kern_y++) {
                 // Only apply kernel values within ppm grid
-                if ((ppm_y + kern_y) >= 0 && (ppm_y + kern_y) < height) {
+                if ((ppm_y + kern_y) < height) {
                     for (int kern_x = -1; kern_x <= 1; kern_x++) {
                         // Only apply kernel values within ppm grid
-                        if ((ppm_x + kern_x) >= 0 && (ppm_x + kern_x) < width){
+                        if ((ppm_x + kern_x) < width){
                             uint8_t pixel = grayscale_image[(kern_y + ppm_y) * width + (ppm_x + kern_x)];
                             // + 1 to correct the indexing for the kernel matrix
                             sum_vertical += kern_vertical[kern_y + 1][kern_x + 1] * pixel;
@@ -53,4 +58,10 @@ void sobel( const uint8_t* img, size_t width, size_t height,
             result[ppm_y * width + ppm_x] = (uint8_t)magnitude;
         }
     }
+
+    // End time measurement
+    struct timespec end;
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double time = (double) (end.tv_sec - start.tv_sec) + 1e-9 * (double) (end.tv_nsec - start.tv_nsec);
+    printf("Time elapsed: %f seconds\n", time);
 }
